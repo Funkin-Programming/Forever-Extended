631c8af0f3cf1c74e7bd062a5e57577da59e508d
Brenninho <157061485+Brenninho123@users.noreply.github.com>
2026-05-10 12:25:43 -0300

    Refactor Main class and add WeekData typedef
    Refactor Main class to improve structure and readability, including new typedef for WeekData and adjustments to game initialization methods.

--- source/Main.hx
+++ source/Main.hx
@@ -1,13 +1,11 @@
 package;
 
-import flixel.FlxBasic;
 import flixel.FlxG;
 import flixel.FlxGame;
-import flixel.FlxSprite;
 import flixel.FlxState;
 import flixel.util.FlxColor;
-import haxe.CallStack.StackItem;
 import haxe.CallStack;
+import haxe.CallStack.StackItem;
 import haxe.io.Path;
 import lime.app.Application;
 import meta.*;
@@ -15,225 +13,165 @@ import meta.data.PlayerSettings;
 import meta.data.dependency.Discord;
 import openfl.Assets;
 import openfl.Lib;
-import openfl.display.FPS;
 import openfl.display.Sprite;
+import openfl.display.StageAlign;
+import openfl.display.StageScaleMode;
 import openfl.events.Event;
 import openfl.events.UncaughtErrorEvent;
+#if sys
 import sys.FileSystem;
 import sys.io.File;
+#end
+#if desktop
 import sys.io.Process;
+#end
+#if android
+import lime.system.System as LimeSystem;
+#end
 
-// Here we actually import the states and metadata, and just the metadata.
-// It's nice to have modularity so that we don't have ALL elements loaded at the same time.
-// at least that's how I think it works. I could be stupid!
+typedef WeekData =
+{
+	var songs:Array<String>;
+	var characters:Array<String>;
+	var color:FlxColor;
+	var name:String;
+}
+
 class Main extends Sprite
 {
-	/*
-		This is the main class of the project, it basically connects everything together.
-		If you know what you're doing, go ahead and shoot! if you're looking for something more specific, however,
-		try accessing some game objects or meta files, meta files control the information (say what's playing on screen)
-		and game objects are like the boyfriend, girlfriend and the oppontent. 
+	public static inline final GAME_WIDTH:Int = 1280;
+	public static inline final GAME_HEIGHT:Int = 720;
+	public static inline final FRAMERATE_CAP:Int = 120;
+	public static inline final FRAMERATE_MOBILE:Int = 60;
+	public static inline final VERSION:String = '0.2.7.1';
 
-		Thanks for using my little modular engine project! I really appreciate it. 
-		If you've got any suggestions let me know at Shubs#0404 on discord or create a ticket on the github.
+	public static var framerate:Int = FRAMERATE_CAP;
+	public static var mainClassState:Class<FlxState> = Init;
+	public static var lastState:FlxState;
+	public static var storageDirectory:String;
+	public static var crashDirectory:String;
 
-		To run through the basics, I've essentially created a rewrite of Friday Night Funkin that is supposed to be
-		more modular for mod devs to use if they want to, as well as to give mod devs a couple legs up in terms of
-		things like organisation and such, since I haven't really seen any engines that are organised like this.
-		also, playstate was getting real crowded so I did a me and decided to rewrite everything instead of just
-		fixing the problems with FNF :P
-
-		yeah this is a problem I have
-		it has to be perfect or else it isn't presentable
-
-		I'm sure I'll write this down in the github, but this is an open source Friday Night Funkin' Modding engine
-		which is completely open for anyone to modify. I have a couple of requests and prerequisites however, and that is
-		that you, number one, in no way claim this engine as your own. If you're going to make an open source modification to the engine
-		you should run a pull request or fork and not create a new standalone repo for it. If you're actually going to mod the game however,
-		please, by all means, create your own repository for it instead as it would be your project then. I also request the engine is credited
-		somewhere in the project. (in the gamebanana page, wherever you'd like/is most convenient for you!)
-		if you don't wanna credit me that's fine, I just ask for the project to be in the credits somewhere 
-		I do ask that you credit me if you make an actual modification to the engine or something like that, basically what I said above
-
-		I have no idea how licenses work so pretend I'm professional or something AAAA
-		thank you for using this engine it means a lot to me :)
-
-		if you have any questions like I said, shoot me a message or something, I'm totally cool with it even if it's just help with programming or something
-		>	fair warning I'm not a very good programmer
-	 */
-	// class action variables
-	public static var gameWidth:Int = 1280; // Width of the game in pixels (might be less / more in actual pixels depending on your zoom).
-	public static var gameHeight:Int = 720; // Height of the game in pixels (might be less / more in actual pixels depending on your zoom).
-
-	public static var mainClassState:Class<FlxState> = Init; // Determine the main class state of the game
-	public static var framerate:Int = 120; // How many frames per second the game should run at.
-
-	public static var gameVersion:String = '0.2.4.2';
-
-	var zoom:Float = -1; // If -1, zoom is automatically calculated to fit the window dimensions.
-	var skipSplash:Bool = true; // Whether to skip the flixel splash screen that appears in release mode.
-	var infoCounter:InfoHud; // initialize the heads up display that shows information before creating it.
-
-	// heres gameweeks set up!
-
-	/**
-		Small bit of documentation here, gameweeks are what control everything in my engine
-		this system will eventually be overhauled in favor of using actual week folders within the 
-		assets.
-		Enough of that, here's how it works
-		[ [songs to use], [characters in songs], [color of week], name of week ]
-	**/
-	public static var gameWeeks:Array<Dynamic> = [
-		[['Tutorial'], ['gf'], [FlxColor.fromRGB(129, 100, 223)], 'Funky Beginnings'],
-		[
-			['Bopeebo', 'Fresh', 'Dadbattle'],
-			['dad', 'dad', 'dad'],
-			[FlxColor.fromRGB(129, 100, 223)],
-			'vs. DADDY DEAREST'
-		],
-		[
-			['Spookeez', 'South', 'Monster'],
-			['spooky', 'spooky', 'monster'],
-			[FlxColor.fromRGB(30, 45, 60)],
-			'Spooky Month'
-		],
-		[
-			['Pico', 'Philly-Nice', 'Blammed'],
-			['pico'],
-			[FlxColor.fromRGB(111, 19, 60)],
-			'vs. Pico'
-		],
-		[
-			['Satin-Panties', 'High', 'Milf'],
-			['mom'],
-			[FlxColor.fromRGB(203, 113, 170)],
-			'MOMMY MUST MURDER'
-		],
-		[
-			['Cocoa', 'Eggnog', 'Winter-Horrorland'],
-			['parents-christmas', 'parents-christmas', 'monster-christmas'],
-			[FlxColor.fromRGB(141, 165, 206)],
-			'RED SNOW'
-		],
-		[
-			['Senpai', 'Roses', 'Thorns'],
-			['senpai', 'senpai', 'spirit'],
-			[FlxColor.fromRGB(206, 106, 169)],
-			"hating simulator ft. moawling"
-		],
+	public static var gameWeeks:Array<WeekData> = [
+		{songs: ['Tutorial'], characters: ['gf'], color: FlxColor.fromRGB(129, 100, 223), name: 'Funky Beginnings'},
+		{songs: ['Bopeebo', 'Fresh', 'Dadbattle'], characters: ['dad', 'dad', 'dad'], color: FlxColor.fromRGB(129, 100, 223), name: 'vs. DADDY DEAREST'},
+		{songs: ['Spookeez', 'South', 'Monster'], characters: ['spooky', 'spooky', 'monster'], color: FlxColor.fromRGB(30, 45, 60), name: 'Spooky Month'},
+		{songs: ['Pico', 'Philly-Nice', 'Blammed'], characters: ['pico', 'pico', 'pico'], color: FlxColor.fromRGB(111, 19, 60), name: 'vs. Pico'},
+		{songs: ['Satin-Panties', 'High', 'Milf'], characters: ['mom', 'mom', 'mom'], color: FlxColor.fromRGB(203, 113, 170), name: 'MOMMY MUST MURDER'},
+		{
+			songs: ['Cocoa', 'Eggnog', 'Winter-Horrorland'],
+			characters: ['parents-christmas', 'parents-christmas', 'monster-christmas'],
+			color: FlxColor.fromRGB(141, 165, 206),
+			name: 'RED SNOW'
+		},
+		{songs: ['Senpai', 'Roses', 'Thorns'], characters: ['senpai', 'senpai', 'spirit'], color: FlxColor.fromRGB(206, 106, 169), name: 'hating simulator ft. moawling'},
 	];
 
-	// most of these variables are just from the base game!
-	// be sure to mess around with these if you'd like.
+	private var infoCounter:InfoHud;
 
 	public static function main():Void
-	{
 		Lib.current.addChild(new Main());
-	}
 
-	// calls a function to set the game up
 	public function new()
 	{
 		super();
+		resolveStoragePaths();
+		registerErrorHandler();
+		resolveFramerate();
+		setupStage();
+		mountGame();
+		bootSystems();
+	}
 
-		/**
-			ok so, haxe html5 CANNOT do 120 fps. it just cannot.
-			so here i just set the framerate to 60 if its complied in html5.
-			reason why we dont just keep it because the game will act as if its 120 fps, and cause
-			note studders and shit its weird.
-		**/
+	private function resolveStoragePaths():Void
+	{
+		#if android
+		storageDirectory = LimeSystem.applicationStorageDirectory;
+		#elseif ios
+		storageDirectory = LimeSystem.applicationStorageDirectory;
+		#else
+		storageDirectory = Sys.getCwd();
+		if (!storageDirectory.endsWith("/") && !storageDirectory.endsWith("\\"))
+			storageDirectory += "/";
+		#end
+		crashDirectory = storageDirectory + "crash/";
+	}
 
+	private function registerErrorHandler():Void
 		Lib.current.loaderInfo.uncaughtErrorEvents.addEventListener(UncaughtErrorEvent.UNCAUGHT_ERROR, onCrash);
 
-		#if html5
-		framerate = 60;
+	private function resolveFramerate():Void
+	{
+		#if mobile
+		framerate = FRAMERATE_MOBILE;
+		#elseif html5
+		framerate = FRAMERATE_MOBILE;
 		#end
+	}
 
-		// simply said, a state is like the 'surface' area of the window where everything is drawn.
-		// if you've used gamemaker you'll probably understand the term surface better
-		// this defines the surface bounds
+	private function setupStage():Void
+	{
+		if (stage != null)
+		{
+			stage.scaleMode = StageScaleMode.NO_SCALE;
+			stage.align = StageAlign.TOP_LEFT;
+		}
+	}
 
+	private function mountGame():Void
+	{
 		var stageWidth:Int = Lib.current.stage.stageWidth;
 		var stageHeight:Int = Lib.current.stage.stageHeight;
 
-		if (zoom == -1)
-		{
-			var ratioX:Float = stageWidth / gameWidth;
-			var ratioY:Float = stageHeight / gameHeight;
-			zoom = Math.min(ratioX, ratioY);
-			gameWidth = Math.ceil(stageWidth / zoom);
-			gameHeight = Math.ceil(stageHeight / zoom);
-			// this just kind of sets up the camera zoom in accordance to the surface width and camera zoom.
-			// if set to negative one, it is done so automatically, which is the default.
-		}
+		var ratioX:Float = stageWidth / GAME_WIDTH;
+		var ratioY:Float = stageHeight / GAME_HEIGHT;
+		var zoom:Float = Math.min(ratioX, ratioY);
 
-		// here we set up the base game
-		var gameCreate:FlxGame;
-		gameCreate = new FlxGame(gameWidth, gameHeight, mainClassState, zoom, framerate, framerate, skipSplash);
-		addChild(gameCreate); // and create it afterwards
+		var gameWidth:Int = Math.ceil(stageWidth / zoom);
+		var gameHeight:Int = Math.ceil(stageHeight / zoom);
 
-		// default game FPS settings, I'll probably comment over them later.
-		// addChild(new FPS(10, 3, 0xFFFFFF));
-
-		// begin the discord rich presence
-		#if !html5
-		Discord.initializeRPC();
-		Discord.changePresence('');
-		#end
-
-		// test initialising the player settings
-		PlayerSettings.init();
-
-		// if you're reading this in the future I've added my own FPS counter below! hopefully...
-		// yeah dw I'm getting started on it fffff
+		var game:FlxGame = new FlxGame(gameWidth, gameHeight, mainClassState, zoom, framerate, framerate, true);
+		addChild(game);
 
 		infoCounter = new InfoHud(10, 3, 0xFFFFFF, true);
 		addChild(infoCounter);
 	}
 
-	public static function framerateAdjust(input:Float)
+	private function bootSystems():Void
 	{
-		return input * (60 / FlxG.drawFramerate);
+		#if desktop
+		Discord.initializeRPC();
+		Discord.changePresence('');
+		#end
+
+		PlayerSettings.init();
 	}
 
-	/*  This is used to switch "rooms," to put it basically. Imagine you are in the main menu, and press the freeplay button.
-		That would change the game's main class to freeplay, as it is the active class at the moment.
-	 */
-	public static var lastState:FlxState;
-
-	public static function switchState(curState:FlxState, target:FlxState)
+	public static function switchState(from:FlxState, to:FlxState):Void
 	{
-		// this is for a dumb feature that has no use except for cool extra info
-		// though I suppose this could be of use to people who want to load things between classes and such
-
-		// save the last state for comparison checks
-		lastState = curState;
-
-		// credit for the idea and a bit of the execution https://github.com/ninjamuffin99/Funkin/pull/1083
-		mainClassState = Type.getClass(target);
-
-		// load the state
-		FlxG.switchState(target);
+		lastState = from;
+		mainClassState = Type.getClass(to);
+		FlxG.switchState(to);
 	}
 
-	public static function updateFramerate(newFramerate:Int)
+	public static function updateFramerate(target:Int):Void
 	{
-		// flixel will literally throw errors at me if I dont separate the orders
-		if (newFramerate > FlxG.updateFramerate)
+		if (target > FlxG.updateFramerate)
 		{
-			FlxG.updateFramerate = newFramerate;
-			FlxG.drawFramerate = newFramerate;
+			FlxG.updateFramerate = target;
+			FlxG.drawFramerate = target;
 		}
 		else
 		{
-			FlxG.drawFramerate = newFramerate;
-			FlxG.updateFramerate = newFramerate;
+			FlxG.drawFramerate = target;
+			FlxG.updateFramerate = target;
 		}
 	}
 
-	public static function dumpCache()
+	public static function framerateAdjust(input:Float):Float
+		return input * (60.0 / FlxG.drawFramerate);
+
+	public static function dumpCache():Void
 	{
-		///* SPECIAL THANKS TO HAYA
 		@:privateAccess
 		for (key in FlxG.bitmap._cache.keys())
 		{
@@ -246,64 +184,66 @@ class Main extends Sprite
 			}
 		}
 		Assets.cache.clear("songs");
-		// */
 	}
-	
 
-	function onCrash(e:UncaughtErrorEvent):Void
+	private function onCrash(e:UncaughtErrorEvent):Void
 	{
-		var errMsg:String = "";
-		var path:String;
 		var callStack:Array<StackItem> = CallStack.exceptionStack(true);
-		var dateNow:String = Date.now().toString();
+		var errLines:Array<String> = [];
 
-		dateNow = StringTools.replace(dateNow, " ", "_");
-		dateNow = StringTools.replace(dateNow, ":", "'");
-
-		path = "./crash/" + "FE_" + dateNow + ".txt";
-
-		for (stackItem in callStack)
+		for (item in callStack)
 		{
-			switch (stackItem)
+			switch (item)
 			{
-				case FilePos(s, file, line, column):
-					errMsg += file + " (line " + line + ")\n";
+				case FilePos(_, file, line, _):
+					errLines.push('$file (line $line)');
 				default:
-					Sys.println(stackItem);
 			}
 		}
 
-		errMsg += "\nUncaught Error: " + e.error + "\nPlease report this error to the GitHub page: https://github.com/Yoshubs/Forever-Engine";
+		errLines.push('');
+		errLines.push('Uncaught Error: ${e.error}');
+		errLines.push('Please report this error to: https://github.com/Funkin-Programming/Forever-Extended');
 
-		if (!FileSystem.exists("./crash/"))
-			FileSystem.createDirectory("./crash/");
+		var errMsg:String = errLines.join('\n');
 
-		File.saveContent(path, errMsg + "\n");
+		#if sys
+		try
+		{
+			var dateNow:String = Date.now().toString();
+			dateNow = StringTools.replace(dateNow, ' ', '_');
+			dateNow = StringTools.replace(dateNow, ':', "'");
 
-		Sys.println(errMsg);
-		Sys.println("Crash dump saved in " + Path.normalize(path));
+			var logPath:String = crashDirectory + 'FE_' + dateNow + '.txt';
 
-		var crashDialoguePath:String = "FE-CrashDialog";
+			if (!FileSystem.exists(crashDirectory))
+				FileSystem.createDirectory(crashDirectory);
 
-		#if windows
-		crashDialoguePath += ".exe";
+			File.saveContent(logPath, errMsg + '\n');
+
+			#if desktop
+			var dialogPath:String = storageDirectory + 'FE-CrashDialog';
+			#if windows
+			dialogPath += '.exe';
+			#end
+			if (FileSystem.exists(dialogPath))
+				new Process(dialogPath, [logPath]);
+			else
+				Application.current.window.alert(errMsg, 'Crash - Forever Engine $VERSION');
+			#else
+			Application.current.window.alert(errMsg, 'Crash - Forever Engine $VERSION');
+			#end
+		}
+		catch (_)
+		{
+			Application.current.window.alert(errMsg, 'Crash - Forever Engine $VERSION');
+		}
+		#else
+		Application.current.window.alert(errMsg, 'Crash - Forever Engine $VERSION');
 		#end
 
-		if (FileSystem.exists("./" + crashDialoguePath))
-		{
-			Sys.println("Found crash dialog: " + crashDialoguePath);
-
-			#if linux
-			crashDialoguePath = "./" + crashDialoguePath;
-			#end
-			new Process(crashDialoguePath, [path]);
-		}
-		else
-		{
-			Sys.println("No crash dialog found! Making a simple alert instead...");
-			Application.current.window.alert(errMsg, "Error!");
-		}
-
+		#if sys
 		Sys.exit(1);
+		#end
 	}
 }
